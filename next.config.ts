414ca85e9c23eb8c035bfdab87ffbc23a770752e
myki97 <pajotmickael@gmail.com>
2026-07-29 20:30:37 +0200

    Fix open-next config and next config for Cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["100.90.186.94"],
   env: {
     NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 };
-
 export default nextConfig;
--- open-next.config.ts
+++ open-next.config.ts
@@ -9,6 +9,7 @@ const config = {
       queue: "dummy",
     },
   },
+  edgeExternals: ["node:crypto"],
   middleware: {
     external: true,
     override: {
@@ -21,5 +22,4 @@ const config = {
     },
   },
 };
-
 export default config;
